5030f0049a62621ad3105a98584bde8e72dedc03
Raj Sharma <163670693+N6si@users.noreply.github.com>
2025-03-21 23:35:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 from flask import Flask, render_template, request
 import joblib
 
-# Load the trained model and vectorizer
 model = joblib.load("fake_review_model.pkl")
 vectorizer = joblib.load("vectorizer.pkl")
 
-# Initialize Flask app
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    prediction = None
+    prediction_text = None
+    prediction_class = None
+
     if request.method == "POST":
         review = request.form["review"]
-        transformed_review = vectorizer.transform([review])  # Convert text to numbers
-        pred = model.predict(transformed_review)  # Predict
-        prediction = "Fake Review 😡" if pred[0] == 1 else "Real Review 😊"
+        transformed_review = vectorizer.transform([review])
+        pred = model.predict(transformed_review)
 
-    return render_template("index.html", prediction=prediction)
+        if pred[0] == 1:
+            prediction_text = "Fake Review 😡"
+            prediction_class = "fake"
+        else:
+            prediction_text = "Real Review 😊"
+            prediction_class = "real"
+
+    return render_template("index.html", prediction_text=prediction_text, prediction_class=prediction_class)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
